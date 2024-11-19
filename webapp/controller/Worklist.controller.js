e3ff4062a3b25bd61e4684251b62d84c844eb250
DashaSidorovich <dashasidorovich2006@gmail.com>
2024-11-19 22:00:12 +0300

    Drag and Drop

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -18,72 +18,121 @@ sap.ui.define([
 				var oViewModel,
 					iOriginalBusyDelay,
 					oTable = this.byId("table");
+				
+				 var oLocalData = {
+		        zjblessons_base_Headers: [
+		            {
+		                HeaderId: "Header001",
+		                DocumentNumber: "DocumentNumber001",
+		                DocumentDate: "2024-11-19",
+		                PlantText: "PlantText001",
+		                RegionText: "Region1",
+		                Description: "Sample Description 1",
+		                Created: "2024-10-01",
+		                Order: 1 
+		            },
+		            {
+		                HeaderId: "Header002",
+		                DocumentNumber: "DocumentNumber002",
+		                DocumentDate: "2024-11-20",
+		                PlantText: "PlantText002",
+		                RegionText: "Region2",
+		                Description: "Sample Description 4",
+		                Created: "2024-10-02",
+		                Order: 4
+		            },
+		            {
+		                HeaderId: "Header003",
+		                DocumentNumber: "DocumentNumber003",
+		                DocumentDate: "2024-11-20",
+		                PlantText: "PlantText003",
+		                RegionText: "Region3",
+		                Description: "Sample Description 3",
+		                Created: "2024-10-02",
+		                Order: 3
+		            },
+		            {
+		                HeaderId: "Header004",
+		                DocumentNumber: "DocumentNumber004",
+		                DocumentDate: "2024-11-20",
+		                PlantText: "PlantText004",
+		                RegionText: "Region4",
+		                Description: "Sample Description 4",
+		                Created: "2024-10-02",
+		                Order: 2
+		            },
+		            {
+		                HeaderId: "Header005",
+		                DocumentNumber: "DocumentNumber005",
+		                DocumentDate: "2024-11-20",
+		                PlantText: "PlantText005",
+		                RegionText: "Region5",
+		                Description: "Sample Description 5",
+		                Created: "2024-10-02",
+		                Order: 0
+		            }
+		        ]
+		    };
 
 
 				iOriginalBusyDelay = oTable.getBusyIndicatorDelay();
 				this._aTableSearchState = [];
 
-				oViewModel = new JSONModel({
-					worklistTableTitle : this.getResourceBundle().getText("worklistTableTitle"),
-					shareOnJamTitle: this.getResourceBundle().getText("worklistTitle"),
-					shareSendEmailSubject: this.getResourceBundle().getText("shareSendEmailWorklistSubject"),
-					shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [location.href]),
-					tableNoDataText : this.getResourceBundle().getText("tableNoDataText"),
-					tableBusyDelay : 0
-				});
-				this.setModel(oViewModel, "worklistView");
+				var oLocalModel = new JSONModel(oLocalData);
+			    this.getView().setModel(oLocalModel);
+			
+			    oViewModel = new JSONModel({
+			        worklistTableTitle: this.getResourceBundle().getText("worklistTableTitle"),
+			        shareOnJamTitle: this.getResourceBundle().getText("worklistTitle"),
+			        shareSendEmailSubject: this.getResourceBundle().getText("shareSendEmailWorklistSubject"),
+			        shareSendEmailMessage: this.getResourceBundle().getText("shareSendEmailWorklistMessage", [location.href]),
+			        tableNoDataText: this.getResourceBundle().getText("tableNoDataText"),
+			        tableBusyDelay: 0
+			    });
+			    this.setModel(oViewModel, "worklistView");
 
 				oTable.attachEventOnce("updateFinished", function(){
 					oViewModel.setProperty("/tableBusyDelay", iOriginalBusyDelay);
 				});
 			},
-			onBeforeRendering: function (oEvent){
-				this._bindTable();
-			},
 			
-			_bindTable: function () {
-				var oTable = this.getView().byId('table');
+			onDropSelectedProductsTable: function (oEvent) {
+				    var oDraggedItem = oEvent.getParameter("draggedControl"); 
+				    var oDroppedItem = oEvent.getParameter("droppedControl"); 
+				    var sDropPosition = oEvent.getParameter("dropPosition"); 
 				
-				oTable.bindItems({
-					path: '/zjblessons_base_Headers',
-					template: this._getTableTemplate(),
-
-					urlParameters: {
-						$select: 'HeaderId, DocumentNumber, DocumentDate, PlantText, RegionText, Description, Created'
-					},
+				    var oModel = this.getView().getModel();
+				    var aData = oModel.getProperty("/zjblessons_base_Headers");
 				
-				});
-			},
+				    var oDraggedData = oDraggedItem.getBindingContext().getObject();
+				
+				    var oDroppedData = oDroppedItem.getBindingContext().getObject();
+				
+				    var iDraggedOrder = oDraggedData.Order;
+				    var iDroppedOrder = oDroppedData.Order;
+				
+				    var iNewOrder = sDropPosition === "After" ? iDroppedOrder + 1 : iDroppedOrder;
+				
+				    oDraggedData.Order = iNewOrder;
+				
+				    aData.forEach(function (item) {
+				        if (item.HeaderId !== oDraggedData.HeaderId) {
+				            if (item.Order >= iNewOrder && iDraggedOrder > iNewOrder) {
+				                item.Order++;
+				            } else if (item.Order <= iNewOrder && iDraggedOrder < iNewOrder) {
+				                item.Order--;
+				            }
+				        }
+				    });
+				
+				    oModel.setProperty("/zjblessons_base_Headers", aData);
+				
+				    this.byId("table").getBinding("items").sort(new Sorter("Order", false));
+				},
 
 			
-			_getTableTemplate: function(){
-				var oTemplate = new sap.m.ColumnListItem({
-					highlight: "{= ${DocumentNumber} !== null ? 'Success' : 'Error'}",
-					type: 'Navigation',
-					cells: [
-						
-						new sap.m.Text({
-							text: '{DocumentNumber}'
-						}),
-						new sap.m.Text({
-							text: '{DocumentDate}'
-						}),
-						new sap.m.Text({
-							text: '{PlantText}'
-						}),
-						new sap.m.Text({
-							text: '{RegionText}'
-						}),
-						new sap.m.Text({
-							text: '{Description}'
-						}),
-						new sap.m.Text({
-							text: '{Created}'
-						})
-						]
-				});
-				return oTemplate;
-			},
+			
+
 			
 			onPressGroup(oEvent){
 				this.getView().byId("table").getBinding("items").sort([new Sorter('RegionText', true, true)]);
